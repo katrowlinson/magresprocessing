e085b2003e9c5d7a1d9aec65239355837fd66bef
katrowlinson <katherine.rowlinson@gmail.com>
2019-03-20 11:49:04 +0000

    cleaning

--- hetcor_sim.py
+++ hetcor_sim.py
@@ -1,3 +1,18 @@
 import scripts.test as test
 test.working()
 
+""" Cleans Mercury data 
+Cleans and references .magres output
+Plots 2D NMR spectra with data to visulise correlation between nuclei via dipolar coupling
+On hover displays atom labels for correlated nuclei
+"""
+
+import scripts.clean_contacts as clean
+
+csv_input = './test_data/contacts_inter.csv' # define path to contacts.csv
+csv_output = './test_data/clean_contacts_inter.csv' # where to save file
+
+array = clean.open_csv(csv_input)
+del array[0]
+print(array)
+clean.write_clean_array(array, csv_output)
--- scripts/clean_contacts.py
+++ scripts/clean_contacts.py
@@ -3,18 +3,18 @@ Script to clean Mercury generated .csv contacts data
 '''
 import csv
 
-csv_path = './1,2-dichlorobenzene/free/inter_contacts.csv' # define path to contacts.csv
-csv_save = './1,2-dichlorobenzene/free/clean_contacts_inter.csv' # where to save file
-array = []
 contact_array = [['Number','Atom1','Atom2','Length']]
 bool = []
 
+# Opens csv file and writes to an array
 def open_csv(csv_path):
     with open(csv_path, 'r') as sim_file:
         sim_reader = csv.reader(sim_file, delimiter=',')
         for row in sim_reader:
             array.append(row[0:4])
+        return array
 
+# Checks that all C labels are in one column and H shifts in the other
 def check(array):
     for i in array:
         if 'C' in i[1] or 'H' in i[2]:
@@ -26,21 +26,22 @@ def check(array):
     else:
         return False
 
-def clean(x, string):
-    x = x.replace(string, '', 1)
+# Cleans string x from substring
+def clean(x, substring):
+    x = x.replace(substring, '', 1)
     return x
 
-#needs to change to while loop bc otherwise counter is going up as list is shrinking
+# Removes contacts data for atoms not available in shift data
 def over_max(array):
     for i in array:
         h_int = int(clean(i[1], 'H'))
         c_int = int(clean(i[2], 'C'))
-        if h_int < 169 and c_int <109: #change based on shift data available
+        if h_int < 353 and c_int <221: #change based on shift data available
             contact_array.append(i)
         else:
             pass
 
-# Makes sure atoms are in the correct columns
+# If labels in incorrect column, switches the labels
 def correct_columns(array):
     for i in array:
         if 'C' in i[1]:
@@ -51,13 +52,15 @@ def correct_columns(array):
         else:
             pass
 
+# Writes clean contacts data to CSV
 def write_clean_csv(csv_save, contact_array):
     with open(csv_save, 'w') as csvFile:
         writer = csv.writer(csvFile)
         writer.writerows(contact_array)
     csvFile.close()
 
-def write_clean_array(array):
+# Writes clean contacts data to an array
+def write_clean_array(array, csv_save):
     if check(array):
         over_max(array)
         write_clean_csv(csv_save, contact_array)
@@ -65,7 +68,3 @@ def write_clean_array(array):
         correct_columns(array)
         over_max(array)
         write_clean_csv(csv_save, contact_array)
-
-open_csv(csv_path)
-del array[0]
-write_clean_array(array)
